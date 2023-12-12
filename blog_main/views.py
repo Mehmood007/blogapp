b5e057a7e4117edcd03da354e0b7d3cbd1f56311
Mehmood Hassan Shah <mehmoodhassan595@yahoo.com>
2023-12-12 19:15:53 +0500

    Blogs are shown dynamically

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from blogs.models import Blog, Category
+
 
 def home(request: HttpRequest) -> render:
-    return render(request, "home.html")
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True, status=1)
+    posts = Blog.objects.filter(is_featured=False, status=1)
+    context = {
+        "categories": categories,
+        "featured_posts": featured_posts,
+        "posts": posts,
+    }
+    return render(request, "home.html", context)
